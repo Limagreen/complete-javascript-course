84fb90711ffb8ffbe2c4488ed6b6c3869ef1010e
Limagreen <cpgreen.us@gmail.com>
2022-05-25 19:23:42 -0400

    Coding Challenge #2

--- Coding Challenges/script.js
+++ Coding Challenges/script.js
@@ -1,8 +1,35 @@
 /* Coding Challenge 1 */
 
-let markHigherBMI;
+// let markHigherBMI;
 
-// Test 1
+// // Test 1
+
+// let markMass = 78; // mass is measured in kilograms
+// let markHeight = 1.69; // height is measured in meters
+// let markBMI = markMass / (markHeight ** 2); // BMI == mass / height^2
+
+// let johnMass = 92;
+// let johnHeight = 1.95
+// let johnBMI = johnMass / (johnHeight ** 2);
+
+// markHigherBMI = markBMI > johnBMI;
+// console.log("Test 1:", markHigherBMI);
+
+// // Test 2
+
+// markMass = 95;
+// markHeight = 1.88;
+// markBMI = markMass / (markHeight ** 2);
+
+// johnMass = 85;
+// johnHeight = 1.76
+// johnBMI = johnMass / (johnHeight ** 2);
+
+// markHigherBMI = markBMI > johnBMI;
+// console.log("Test 2:", markHigherBMI);
+
+
+/* Coding Challenge 2 */
 
 let markMass = 78; // mass is measured in kilograms
 let markHeight = 1.69; // height is measured in meters
@@ -12,8 +39,9 @@ let johnMass = 92;
 let johnHeight = 1.95
 let johnBMI = johnMass / (johnHeight ** 2);
 
-markHigherBMI = markBMI > johnBMI;
-console.log("Test 1:", markHigherBMI);
+if (markBMI > johnBMI) console.log(`Test 1: Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+else console.log(`Test 1: John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+
 
 // Test 2
 
@@ -25,6 +53,5 @@ johnMass = 85;
 johnHeight = 1.76
 johnBMI = johnMass / (johnHeight ** 2);
 
-markHigherBMI = markBMI > johnBMI;
-console.log("Test 2:", markHigherBMI);
-
+if (markBMI > johnBMI) console.log(`Test 1: Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+else console.log(`Test 1: John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
