cdd0df7696ba139cc9b8291281fda2fd68bea0ba
Limagreen <cpgreen.us@gmail.com>
2022-05-25 18:03:56 -0400

    5/25/2022 6:03 PM
    Completed up to Coding Challenge #1 in Fundamentals Part 1

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,65 @@
+/* Assignment 1: Values and Variables */
+
+// // create variables
+// let country = "United States", continent = "North America";
+// let population = 330149796;
+
+// // display them on the console
+// console.log("Assignment 1");
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+
+/* Assignment 2: Data Types */
+
+// // Variable creation
+// let isIsland = false;
+// let language; // purposefully left undefined
+
+// // display data types on console
+// console.log("Assignment 2");
+// console.log("Country: " + typeof country);
+// console.log("isIsland: " + typeof isIsland);
+// console.log("Population: " + typeof population);
+// console.log("Language: " + typeof language);
+
+
+/* Assignment 3: Let, Const, and Var */
+
+// // re-declare variables as const where applicable
+// const country = "United States";
+// const continent = "North America";
+// const isIsland = false;
+// let population = 330149796;
+// const language = "English";
+
+
+/* Assignment 4: Basic Operators */
+
+// // 1.
+// let population = 330149796;
+// console.log(population / 2, "people would live in each half.");
+
+// // 2.
+// population++;
+// console.log(population);
+
+// // 3.
+// const finlandPopulation = 6000000;
+// console.log(population > finlandPopulation);
+
+// // 4.
+// const avgPopulation = 33000000;
+// console.log(population > avgPopulation);
+
+// // 5.
+// const country = "United States";
+// const continent = "North America";
+// const language = "English";
+
+// const description = "The " + country + " is in " + continent + ", and its " + population + " people speak " + language;
+// console.log(description);
+
+
+/* */
