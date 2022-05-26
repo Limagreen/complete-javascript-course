f7b6f224945281cf49308732151c5400668bd7b0
Limagreen <cpgreen.us@gmail.com>
2022-05-26 19:23:42 -0400

    Assignment 8: Equality Operators == vs. ===

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -94,8 +94,29 @@
 */
 
 // 2. Code
-console.log('9' - '5');
-console.log('19' - '13' + '17');
-console.log('19' - '13' + 17);
-console.log('123' < 57);
-console.log(5 + 6 + '4' + 9 - 4 - 2);
+// console.log('9' - '5');
+// console.log('19' - '13' + '17');
+// console.log('19' - '13' + 17);
+// console.log('123' < 57);
+// console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+
+/* Assignment 8: Equality Operators == vs. === */
+
+// 1.
+const numNeighbors = prompt(`How many neighbor countries does your country have?`);
+
+// 2.
+if (numNeighbors == 1) console.log(`(Loose) Only 1 border!`);
+
+// 3.
+else if (numNeighbors >= 1) console.log(`More than 1 border.`);
+
+// 4.
+else console.log(`No borders.`);
+
+// 6. 
+if (numNeighbors === 1) console.log(`(Strict) Only 1 border!`);
+
+// 7.
+if (Number(numNeighbors) === 1) console.log(`(Strict, Manual Conversion) Only 1 border!`);
