067ab13fb6d4f35322d794c4f9bd4fe0a414bf5c
Limagreen <cpgreen.us@gmail.com>
2022-05-26 20:57:07 -0400

    Coding Challenge 3

--- Coding Challenges/script.js
+++ Coding Challenges/script.js
@@ -31,27 +31,85 @@
 
 /* Coding Challenge 2 */
 
-let markMass = 78; // mass is measured in kilograms
-let markHeight = 1.69; // height is measured in meters
-let markBMI = markMass / (markHeight ** 2); // BMI == mass / height^2
+// // Test 1
 
-let johnMass = 92;
-let johnHeight = 1.95
-let johnBMI = johnMass / (johnHeight ** 2);
+// let markMass = 78; // mass is measured in kilograms
+// let markHeight = 1.69; // height is measured in meters
+// let markBMI = markMass / (markHeight ** 2); // BMI == mass / height^2
 
-if (markBMI > johnBMI) console.log(`Test 1: Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
-else console.log(`Test 1: John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+// let johnMass = 92;
+// let johnHeight = 1.95
+// let johnBMI = johnMass / (johnHeight ** 2);
 
+// if (markBMI > johnBMI) console.log(`Test 1: Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+// else console.log(`Test 1: John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+
+
+// // Test 2
+
+// markMass = 95;
+// markHeight = 1.88;
+// markBMI = markMass / (markHeight ** 2);
+
+// johnMass = 85;
+// johnHeight = 1.76
+// johnBMI = johnMass / (johnHeight ** 2);
+
+// if (markBMI > johnBMI) console.log(`Test 1: Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+// else console.log(`Test 1: John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+
+
+/* Coding Challenge 3 */
+
+// Test 1
+
+let dolphinsScores = [96, 108, 89];
+let koalasScores = [88, 91, 110];
+
+let dolphinsAvg = calculateAvg(dolphinsScores);
+let koalasAvg = calculateAvg(koalasScores);
+
+determineWinner(dolphinsAvg, koalasAvg);
 
 // Test 2
 
-markMass = 95;
-markHeight = 1.88;
-markBMI = markMass / (markHeight ** 2);
+dolphinsScores = [97, 112, 101];
+koalasScores = [109, 95, 123];
 
-johnMass = 85;
-johnHeight = 1.76
-johnBMI = johnMass / (johnHeight ** 2);
+dolphinsAvg = calculateAvg(dolphinsScores);
+koalasAvg = calculateAvg(koalasScores);
 
-if (markBMI > johnBMI) console.log(`Test 1: Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
-else console.log(`Test 1: John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+determineWinner(dolphinsAvg, koalasAvg);
+
+// Test 3
+
+dolphinsScores = [97, 112, 101];
+koalasScores = [109, 95, 106];
+
+dolphinsAvg = calculateAvg(dolphinsScores);
+koalasAvg = calculateAvg(koalasScores);
+
+determineWinner(dolphinsAvg, koalasAvg);
+
+// Functions
+
+function calculateAvg(scores) {
+
+    let sum = 0;
+
+    scores.forEach(function (score) {
+        sum += score;
+    });
+
+    return sum / scores.length;
+}
+
+function determineWinner(dolphinsAvg, koalasAvg) {
+    if (dolphinsAvg > 100 && dolphinsAvg > koalasAvg) console.log(`The Dolphins win!`);
+
+    else if (koalasAvg > 100 && koalasAvg > dolphinsAvg) console.log(`The Koalas win!`);
+
+    else if (dolphinsAvg > 100 && koalasAvg > 100 && koalasAvg === dolphinsAvg) console.log(`The Dolphins and Koalas tied!`);
+
+    else console.log(`Neither team won.`);
+}
